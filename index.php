d11cac15b89787658aee4361ea2c646f548f6f80
Bogdan Sorin <tzully.007@gmail.com>
2012-01-10 11:24:32 +0200

    added sessions

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 				<a href = "index.php">Home</a> | 
 				<a href = "faq.php">FAQ</a> | 
 				<a href = "about.php">About</a> | 
-				<?php if ($user): ?>
+				<?php if ($_SESSION['logat']): ?>
 					<a href = "profile.php">Profile</a> | 
 					<a href = "map.php">Map</a> | 
 					<a href = "top.php">Top</a> | 
@@ -28,7 +28,27 @@
 			</div>
 			<div class = "container">
 				<div class = "profile">
-				
+					<?php if ($_SESSION['logat']): ?>
+						<table>
+							<tr>
+								<td>Name:</td>
+								<td><?php echo $_SESSION['name']; ?></td>
+							<tr>
+								<td>UiD:</td>
+								<td><?php echo $_SESSION['uid']; ?></td>
+							<tr>
+								<td>Exp:</td>
+								<td><?php echo $_SESSION['exp']; ?></td>
+							<tr>
+								<td>Lvl:</td>
+								<td><?php echo $_SESSION['lvl']; ?></td>
+							<tr>
+								<td>Money:</td>
+								<td><?php echo $_SESSION['money']; ?></td>
+						</table>
+					<?php else: ?>
+						<p>You are not connected</p>
+					<?php endif ?>
 				</div>
 				<div class = "content">
 				
--- login.php
+++ login.php
@@ -1,7 +1,6 @@
 <?php
 	include 'inc/config.php';
 	include 'inc/fb_get.php';
-
 ?>
 <!doctype html>
 <html xmlns:fb="http://www.facebook.com/2008/fbml">
@@ -13,14 +12,30 @@
 	<?php
 	if ($user) // if user is succesfuly connected
 	{
+		$_SESSION['logat'] = true;
 		// Check if user exists in the database
 		$query = "SELECT * FROM `t_users` WHERE id='".$user_profile['id']."'";
 		$result = mysql_query($query, $link);
-		if(mysql_num_rows($result) != 1) // New user
+		$status = (mysql_num_rows($result) != 1) ? true : false; // true - new user ; false - user exists
+		if($status) // New user
 		{
 			$query = "INSERT INTO `t_users` (id, name) VALUES ('".$user_profile['id']."', '".$user_profile['name']."')";
 			mysql_query($query, $link);
+			$_SESSION['money'] = 2500.0;
+			$_SESSION['exp'] = 0;
+			$_SESSION['rank'] = "user";
+			$_SESSION['lvl'] = 0;
 		}
+		else
+		{
+			$row = mysql_fetch_array($result);
+			$_SESSION['money'] = $row['money'];
+			$_SESSION['exp'] = $row['exp'];
+			$_SESSION['rank'] = $row['rank'];
+			$_SESSION['lvl'] = $row['lvl'];
+		}
+		$_SESSION['uid'] = $user_profile['id'];
+		$_SESSION['name'] = $user_profile['name'];
 		echo '<meta http-equiv="refresh" content="1; url=index.php">';
 	}
 	else //user not connected
